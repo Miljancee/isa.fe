9663f18b2757325270675dff47612f4d126bcd00
Miljancee <miljannedeljkovic02@gmail.com>
2024-06-26 23:05:26 +0200

    Frontend

---
+++ src/app/user/create/page.js
@@ -0,0 +1,43 @@
+
+'use client'
+
+import Link from "next/link";
+import {useEffect, useState} from "react";
+import {get} from "@/core/httpClient";
+
+
+export default function UserCreate() {
+    const [counter, setCounter] = useState(0);
+    const [loading, setLoading] = useState(false);
+    const [data, setData] = useState();
+
+
+    const getFirstName = async () =>{
+        setLoading(true);
+        let result = await get("/user/get-first-name");
+
+        setData(result.data);
+        setLoading(false);
+
+    }
+
+    useEffect(() => {
+        getFirstName();
+    }, []);
+
+
+    return (
+
+        <>
+            {loading === true ? <h1>Loading....</h1>: (
+                <>
+                    <h1>{data}</h1>
+
+                </>
+
+            )}
+
+        </>
+    );
+
+}
